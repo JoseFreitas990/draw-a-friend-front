eae1bc325c9528208b1001ff6cdd8104de592750
JoseFreitas990 <freitasze990@gmail.com>
2022-11-10 16:21:48 +0000

    redux-rtk init

---
+++ src/app/store.ts
@@ -0,0 +1,15 @@
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {},
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
